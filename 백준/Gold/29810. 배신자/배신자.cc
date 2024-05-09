830796cc42ef1aa29210a4127108f52814e85959
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-05-10 01:00:09 +0900

    [Gold III] Title: 배신자, Time: 100 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Gold/29810. 배신자/배신자.cc
+++ 백준/Gold/29810. 배신자/배신자.cc
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, x;
+
+int n, m, x, maxcnt = 0;
 vector<int> freinds[200001];
+bool flag1[200001], visited[200001];
 
-// 사이클 형성과 형성 아닌 것들 둘을 다 알아야함
+void input(){
 
-bool flag1[200001];
-void input()
-{
     cin >> n >> m;
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++){
         int a, b;
         cin >> a >> b;
 
@@ -19,22 +17,17 @@ void input()
     }
     cin >> x;
 }
-// 배신자랑 닿아있는 애들 표시해보자 걔네끼리 연결되면 그걸로 dfs
-int maxcnt = 0;
-bool visited[200001];
+// 배신자랑 닿아있는 애들 표시해보자 걔네끼리 연결되면 그걸로
 
-int bfs(int a)
-{
-    int check=0;
-    int flag = 0;
-    int cnt = 1;
+int bfs(int a){
+
+    int check=0, flag = 0, cnt = 1;
+
     queue<int> q;
-    q = queue<int>();
     q.push(a);
-    visited[a]=1;
+    
+    while (!q.empty()){
 
-    while (!q.empty())
-    {
         int b = q.front();
         q.pop();
         if(flag1[b]==1){
@@ -43,21 +36,19 @@ int bfs(int a)
                 cnt--;
             }
         }
-        for (int i = 0; i < freinds[b].size(); i++)
-        {
+        for (int i = 0; i < freinds[b].size(); i++){
             int c = freinds[b][i];
         
-            if (c != x)
-            {
-                if (visited[c] == 0)
-                {
+            if (c != x){
+
+                if (visited[c] == 0){
+                
                     visited[c] = 1;
                     q.push(c);
                     cnt++;
                 }
             }
-            else
-            {
+            else{
                 if(!flag)
                     cnt++;
                 flag=1;
@@ -67,25 +58,25 @@ int bfs(int a)
     return cnt;
 }
 
-void solve()
-{
-    for (int i = 0; i < freinds[x].size(); i++)
-    {
+void solve(){
+
+    for (int i = 0; i < freinds[x].size(); i++){
+
         flag1[freinds[x][i]] = 1; // true 면 배신자랑 닿아있음
     }
     for (int i = 1; i <= n; i++)
     {
-        if (visited[i] == 0 && i != x)
-        {
-            int a=bfs(i);
-            maxcnt = max(maxcnt, a);
+        if (visited[i] == 0 && i != x){
+
+            visited[i]=1;
+            maxcnt = max(maxcnt, bfs(i));
         }
     }
     cout << maxcnt;
 }
 
-int main()
-{
+int main(){
+    
     ios::sync_with_stdio(false);
     cin.tie(0);
     input();

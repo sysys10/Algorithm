7a3dd5d05e9a1c9674c861bf82299814187fb792
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-05-18 15:30:44 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n,k;
+
+   cin>>n;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin>>k;
+
+        for(int j = 0 ; j < k ; j++)
+        {
+            cout<<'=';
+        }
+        cout<<endl;
+
+    }
+
+}

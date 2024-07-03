67d2ed95a2969b9f028e2200c2753536963b90ff
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-07-03 19:29:28 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int q, w, e, r, t;
+    cin >> q >> w >> e >> r >> t;
+    int a = q * w;
+    int b = e * r * t;
+    cout << a - b;
+}

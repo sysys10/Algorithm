7240ea3b6b2d9ebaa7b83baf8592b9c66efd4527
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-07-03 16:52:49 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string str;
+    cin >> str;
+    if(str=="SONGDO")
+        cout << "HIGHSCHOOL";else
+    if(str=="CODE")
+        cout << "MASTER";else
+    if(str=="2023")
+        cout << "0611";else
+        cout << "CONTEST";
+}

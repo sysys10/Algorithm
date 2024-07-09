ba04c580f44bcca90448a400166c0eabebe7d4bc
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-07-09 21:23:09 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << min(n/2,m/2);
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}

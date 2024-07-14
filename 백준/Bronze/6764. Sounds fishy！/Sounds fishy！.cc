6cab73e591d89699cc06840099459f8144430029
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-07-15 01:18:49 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[4];
+void solve()
+{
+    for (int i = 0; i < 4; i++)
+        cin >> a[i];
+    if (a[0] > a[1] && a[1] > a[2] && a[2] > a[3])
+        cout << "Fish Diving";
+    else if (a[0] < a[1] && a[1] < a[2] && a[2] < a[3])
+        cout << "Fish Rising";
+    else if (a[0] == a[1] && a[1] == a[2] && a[2] == a[3])
+        cout << "Fish At Constant Depth";
+    else
+        cout << "No Fish";
+}
+
+int main()
+{
+    solve();
+}

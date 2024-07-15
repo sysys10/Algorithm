951806bf62157318540fd072899104cc049d8945
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-07-15 19:02:05 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int pmt = b / a;
+    cout << pmt * c;
+}
+
+int main()
+{
+    solve();
+}

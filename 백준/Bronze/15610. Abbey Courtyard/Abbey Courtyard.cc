afa25096af643264d3f951c8efb8aecf8830da4f
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-07-15 18:50:36 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    double square;
+    cin >> square;
+    printf("%.8f", sqrt(square) * 4);
+}
+
+int main()
+{
+    solve();
+}

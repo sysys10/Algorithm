01d7726a92b2b6e214ff41a120070def55106db3
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-07-19 21:01:23 +0900

    [Bronze IV] Title: Time to Decompress, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+    int t;
+    cin >> t;
+    while(t--){
+        int a;
+        char s;
+        cin >> a >> s;
+        while(a--){
+            cout << s;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    solve();
+}

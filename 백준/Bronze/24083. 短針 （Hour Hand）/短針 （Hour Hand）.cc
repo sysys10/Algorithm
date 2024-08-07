9ae843af9bfedc51e3d3fc247306349702bc9cb5
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-07 17:37:42 +0900

    [Bronze IV] Title: 短針 (Hour Hand), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24083. 短針 （Hour Hand）/短針 （Hour Hand）.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+
+    cin >> A >> B;
+
+    if ((A + B) % 12 != 0)
+        cout << (A + B) % 12 << '\n';
+    else
+        cout << 12 << '\n';
+
+    return 0;
+}

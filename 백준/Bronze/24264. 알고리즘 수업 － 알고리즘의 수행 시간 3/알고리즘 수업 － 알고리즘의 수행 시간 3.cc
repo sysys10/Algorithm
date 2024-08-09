6b21e41f2147631212622322e291daecec3c67d9
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-09 22:03:13 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+
+    cout << n * n;
+    cout << endl << 2;
+}

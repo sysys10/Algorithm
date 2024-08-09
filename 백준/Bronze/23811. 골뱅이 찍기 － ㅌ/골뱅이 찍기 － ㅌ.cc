a1c99504137c7534231fe58074315b46a3252f24
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-09 22:00:07 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+void printg(int i, int d)
+{
+    for (int i = 0; i < n * d; i++)
+    {
+        cout << "@";
+    }
+}
+void printb(int i, int d)
+{
+    for (int i = 0; i < n * d; i++)
+    {
+        cout << " ";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        printg(1, 5);
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printg(1, 1);
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+
+        printg(1, 5);
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printg(1, 1);
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printg(1, 5);
+        cout << endl;
+    }
+}

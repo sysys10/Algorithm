11c82ccfa4d9e43228729a4c279381563ade5ce9
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-13 21:55:50 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+int n, m;
+void solve()
+{
+
+    int n;
+    cin >> n;
+
+    cout << "The largest square has side length " << (int)sqrt(n) << '.';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}

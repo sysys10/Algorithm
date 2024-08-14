84906115deadd01ff75e77a74f195fbe63e95482
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-14 19:08:48 +0900

    [Bronze III] Title: Not A + B, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+int n, m, k;
+void solve()
+{
+    cin >> n;
+    while(n--){
+        int a, b;
+        cin >> a >> b;
+            cout << 1<<endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}

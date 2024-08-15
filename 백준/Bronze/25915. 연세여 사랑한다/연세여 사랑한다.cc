6342492efe7fd197222c3b7a67fa3f8be31125f5
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-15 13:12:29 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, k;
+void solve()
+{
+    char c;
+    cin >> c;
+    cout << 84 + abs(c - 'I');
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}

ecd544e92e5f956e36029fcb06bc9e2ea9815971
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-16 22:15:55 +0900

    [Bronze III] Title: Сапсан, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19771. Сапсан/Сапсан.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+
+    cin >> n;
+
+    int result = n / 6 * 4;
+
+    cout << result;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}

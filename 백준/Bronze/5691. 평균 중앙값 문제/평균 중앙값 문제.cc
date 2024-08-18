46f44fc42a181087309a425c060f83003c529167
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-18 22:39:57 +0900

    [Bronze III] Title: 평균 중앙값 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> v;
+
+void solve()
+{
+    
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+
+    while (1)
+    {
+        cin >> n >> m;
+        if (n == 0 && m == 0)
+            break;
+        cout << n - (m - n) << '\n';
+    }
+
+}
+
+int main()
+{
+    solve();
+}

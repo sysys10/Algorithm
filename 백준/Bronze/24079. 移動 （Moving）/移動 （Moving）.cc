a91f72729da368408dbd8bdf9e9ff9b827c484f5
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-19 14:37:55 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    a + b <= c ? cout << 1 : cout << 0;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}

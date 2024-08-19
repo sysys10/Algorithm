24435b884c01f23b0c5bb21dd2597a70f92b2c44
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-19 14:33:15 +0900

    [Bronze IV] Title: Contemporary Art, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    double n;
+    cin >> n;
+    printf("%.8f", sqrt(n) * 4);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}

18cfb5cf6d1dfcd798ad8dfdae6b3af982f44f5a
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-08-20 16:27:25 +0900

    [Bronze III] Title: Chanukah Challenge, Time: 4 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/15295. Chanukah Challenge/Chanukah Challenge.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> v(100001);
+void solve()
+{
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m;
+        cout << n << ' ' << (m + 1) * (m + 2) / 2 - 1 << '\n';
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}

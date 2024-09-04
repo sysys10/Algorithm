4a0b7192cb6bee7c6695306972218b80e618ada8
shinys10 <166797095+sysys10@users.noreply.github.com>
2024-09-04 22:20:52 +0900

    [Bronze III] Title: Balloons Colors, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9635. Balloons Colors/Balloons Colors.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())) :
+    N, X, Y = map(int, input().split())
+    lst = list(map(int, input().split()))
+    if lst[0] == X and lst[-1] == Y : 
+        print("BOTH")
+    elif lst[0] == X : 
+        print("EASY")
+    elif lst[-1] == Y : 
+        print("HARD")
+    else : 
+        print("OKAY")
